bbdaab79d8febd7eaf7567f431f8dfa40e6fcc7f
Aurélien Flura Home <aflura@osires.fr>
2022-01-09 23:26:12 +0100

    Wip ch5

---
+++ Chapter_5_Higher_Order_Functions/CH5_Summary_And_Exercises.js
@@ -0,0 +1,22 @@
+/**
+ * ABSTRACTION
+ * Il est primordiale pour un developpeur de savoir découper son programme en actions compréhensibles par tous, et qui cachent les détails important peu.
+ * C'est le concept de l'abstraction : ceci permet de parler d'un problème à un niveau plus élevé (ou plus abstrait)
+ *
+ * ABSTRACTING REPETITION
+ *
+ *
+ */
+
+/** EXERCISES */
+
+/** SANDBOX */
+
+
+/** THE SUM OF A RANGE */
+
+
+/** DEEP COMPARISON */
+
+
+
