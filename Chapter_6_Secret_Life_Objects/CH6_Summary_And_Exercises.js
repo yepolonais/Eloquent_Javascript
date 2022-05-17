59d7b11548a989d5ef5d19c25be4e3d171acc673
Aurélien Flura <aflura@osires.fr>
2022-05-17 16:54:35 +0200

    Exo ch6 en cours

--- Chapter_6_Secret_Life_Objects/CH6_Summary_And_Exercises.js
+++ Chapter_6_Secret_Life_Objects/CH6_Summary_And_Exercises.js
@@ -90,19 +90,190 @@
  * Utile lorsque l'on veut créer un objet dont les clés ne sont pas forcément des strings (des objets par exemple!)
  *
  * POLYMORPHISM
+ * L'idée derrière le polymorphisme est qu'un objet supporte une certaine interface.
+ * Si c'est le cas, il pourra s'intégrer sans souci à du code qui expose la même interface, cad des des popriétés et méthodes ayant les mêmes signatures.
  *
+ * SYMBOLS
+ * Le problême derrière ces interfaces est que différentes interfaces peuvent utiliser des méthodes ou prop ayant les mêmes signatures, mais produisant des résultats différents.
+ * Utiliser ces deux interfaces en parallèle créérait un conflit. C'est là qu'interviennent les symboles.
+ * La particularité d'un symbole est qu'il est unique - on ne peut pas créér le même symbole deux fois.
  *
+ * Symbol("name") === Symbol("name"); // => false
  *
+ * Utiliser des symboles pour nommer les propriété d'une interface permet de s'assurer qu'il n'y aura pas de conflit.
+ * Pour les utiliser il faut les noter entre crochet:
+ * const toStringSymbol = Symbol("toString");
+      Array.prototype[toStringSymbol] = function() {
+      return `${this.length} cm of blue yarn`;
+      };
+ *
+ * THE ITERATOR INTERFACE
+ * Lorsque l'on fournit un objet à une boucle for/of, ce dernier doit être itérable.
+ * Cela signifie qu'il doit posséder une méthode nommée avec le symbole [Symbol.iterator].
+ * Ce symbole est défini par JS comme une prop de la fonction Symbol.
+ * Lorsque cette méthode est appelé, elle doit renvoyer un object qui respecte une seconde interface : iterator.
+ * Ce second objet possède une méthode next(), qui renvoie un objet avec les prop value (qui pointe vers la valeur suivante) et done (un booléen indiquant lorsqu'il n'y a plus de valeur suivante).
+ * next, value et done ne sont pas des symboles.
+ * Il est possible d'utiliser l'interface [Symbol.iterator] directement:
+ *
+ * let brochet = "Brochet"[Symbol.iterator]();
+ * brochet.next(); // {value: "B", done: false};
+ * brochet.next(); // {value:"r", done : false}
+ * etc...
+ *
+ * Il est possible de l'intégrer à nos propres objets
+ *
+ * class Matrix {
+      constructor(width, height) {
+         this.width = width;
+         this.height = height;
+      }
+   }
+
+   class MatrixIterator {
+      constructor(matrix) {
+         this.x = 0;
+         this.y = 0;
+         this.matrix = matrix;
+      }
+
+      next() {
+         if ("plus d'objet") return {done: true};
+         let value = "some code";
+         else return {value, done: false};
+      }
+   }
+
+   Matrix.prototype[Symbol.iterator] = function() {
+      return new MatrixIterator(this);
+   };
+ *
+ * GETTER, SETTER AND STATICS
+ * Les getters et les setters sont utilisés pour lire ou écrire une propriété de notre objet, en lui ajoutant une certaine logique.
+ * Les méthodes statiques permettent d'appeler une méthode d'un objet sans avoir besoin de l'instancier. Cela peut être utile pour étendre l'instanciation d'une classe.
+ * class Temperature {
+      constructor(celsius) {
+         this.celsius = celsius;
+      }
+      get fahrenheit() {
+         return this.celsius * 1.8 + 32;
+      }
+      set fahrenheit(value) {
+         this.celsius = (value - 32) / 1.8;
+      }
+
+      static fromFahrenheit(value) {
+         return new Temperature((value - 32) / 1.8);
+      }
+   }
+ *
+ * INHERITANCE
+ * L'héritage consiste à créer un objet qui hérite d'un autre objet (ses propriétés et méthodes).
+ * Il peut surcharger les propriétés de son parent.
+ * Contrairement aux concepts d'encapsulation et polymorphisme qui permettent de décorréler le code, l'héritage le lie.
+ * Il faut donc l'utiliser avec parcimonie.
+ *
+ * THE INSTANCEOF OPERATOR
+ * L'opérateur binaire 'instanceof' permet de savoir si un objet dérive d'une classe particulière.
+ * Il remonte les liens d'héritage
  *
  */
 
 /** EXERCISES */
 
-/** FLATTENING */
+/** SYMBOLS */
 
-let empty = {};
-empty.toString();
-empty.toString;
+let nameSym = Symbol("name");
+let bool = nameSym === Symbol("name");
+let bool2 = Symbol("name") === Symbol("name");
+
+/** A VECTOR TYPE */
+
+class Vec {
+      constructor(x, y) {
+         this.x = x;
+         this.y = y;
+      }
+      plus({x,y}) {
+         return new Vec(this.x + x, this.y + y);
+      }
+      minus({x,y}) {
+         return new Vec(this.x - x, this.y - y);
+      }
+      get length(){
+         return Math.sqrt(this.x**2 + this.y**2)
+      }
+   }
+
+new Vec(1, 2).plus(new Vec(2, 3));
+new Vec(1, 2).minus(new Vec(2, 3));
+new Vec(3, 4).length;
+
+/** GROUP */
+
+class Group{
+   constructor(){
+      this.values = [];
+   }
+   add(item){
+      if(!this.has(item)) this.values.push(item);
+   }
+   delete(item){
+      if(this.has(item)) this.values.splice(this.values.indexOf(item),1);
+   }
+   has(item){
+      if(this.values.indexOf(item) === -1) return false;
+      else return true;
+   }
+   /**
+    *
+    * @param {object} iter
+    * @returns
+    */
+   static from(iter){
+      let newGroup = new Group();
+      for (let item of iter){
+         newGroup.add(item);
+      }
+      return newGroup;
+   }
+
+   // Interface iterable
+   [Symbol.iterator] = function() {
+      return new GroupIterator(this)
+   }
+
+}
 
 
 
+let group = Group.from([10, 20]);
+group.has(10);
+group.has(30)
+group.add(10);
+group.delete(10);
+group.has(10);
+
+/** ITERABLE GROUPS */
+
+class GroupIterator{
+   constructor(group){
+      this.currentIndex = 0;
+      this.group = group;
+   }
+   next(){
+      if((this.currentIndex == this.group.values.length)){
+         return {done: true};
+      }
+      else {
+         let value = this.group.values[this.currentIndex];
+            this.currentIndex += 1;
+         return {value, done: false};
+      }
+
+   }
+}
+
+for (let value of Group.from(["a", "b", "c"])) {
+  value;
+}
