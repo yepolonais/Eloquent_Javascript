9126f8b0ed4d1856386148e25135fe0581685299
Aurélien Flura <aflura@osires.fr>
2022-05-18 18:04:27 +0200

    WIP ch9

--- Chapter_9_Regular_Expressions/CH9_Exercises.js
+++ Chapter_9_Regular_Expressions/CH9_Exercises.js
@@ -1,5 +1,14 @@
 
-//
-let regex = /'[^']*'/;
+// recherche
+let regex = /'([^']*)'/;
 
 regex.exec("She said 'hello'");
+
+
+let notBinary = /[^01]+/;
+notBinary.exec("1100100010100110");
+// → false
+notBinary.exec("1100220010200110");
+// → true
+
+/bad(ly)?/.exec("badly")
