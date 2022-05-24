f6c9798fd61cc2e16ea71032d7901bf7e53b4a90
Aurélien Flura <aflura@osires.fr>
2022-05-24 13:34:53 +0200

    CH10 OK

--- Chapter_10_Modules/CH10_Exercises.js
+++ Chapter_10_Modules/CH10_Exercises.js
@@ -1,1 +1,20 @@
-/** REGEXP GOLF */
+/** ROADS MODULE */
+
+const buildGraph = require('./graph');
+
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+let formatedRoads = roads.map(road => road.split('-'))
+
+exports.roadGraph = buildGraph(formatedRoads);
+
+/** CIRCULAR DEPENDENCIES */
+
